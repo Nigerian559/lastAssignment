a792b78570bcddddaee6a1a05f0a407057d993d6
Olukoya <alexolukoya@my.whccd.edu>
2020-03-11 21:32:24 -0700

    i did my console log statements

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+  "What worked well for you over the last 9 weeks that aided in your learning process?"
+);
+console.log("what worked well was the demos and going back into the assignments we worked on");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("the only thing i would change is how long the course is");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("yes i have more knowledge on computer coding than i had before i came");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "What concepts were new to you and what concepts had you encountered already in the past?"
+);
+console.log("everything was new to me");
